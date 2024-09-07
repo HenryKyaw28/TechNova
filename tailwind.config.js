4a3cdb97f123a24cc3bb7278f3e3279236b9fcf7
HenryKyaw <HenryKyaw28>
2024-09-07 22:46:24 +0630

    Main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"],
   theme: {
     extend: {
-      fontFamily: {
-        primary: ['Open Sans'],
-      },
       colors: {
-        'primary': '#007bff ',
-        'secondary': '#6c757d ',
-        'teritiary': '#f8f9fa ',
+        'forText': '#02040a',
+        'primary': '#007bff ', 
+        'secondary': '#6c757d ', 
+        'tertiary': '#f8f9fa ',
         'accent': '#ff5722 ',
-        },
-      },
+      }, 
+      fontFamily: {
+        openSans: ['Open Sans'], 
+      }
     },
   },
   plugins: [],
